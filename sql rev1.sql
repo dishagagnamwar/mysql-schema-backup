0d3038f424dd6869e27185f3e21049d443a796d3
student apnacollege <dishagagnamwar2711@gmail.com>
2026-09-02 14:46:21 +0530

    sql rev

---
+++ sql rev1.sql
@@ -0,0 +1,8 @@
+show databases;
+use dummy;
+create database disha;
+use disha;
+create table info2(id int , name varchar(50),department varchar(50), phone bigint);
+show tables;
+drop table info; 
+show tables;
